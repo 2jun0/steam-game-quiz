0f6b5bcba453c06afec5597be3353876d18bcc98
2jun0 <soo28819@gmail.com>
2023-11-29 17:39:49 +0900

    feat: add Relationship between Game with GameScreenshot

--- data_scrapers/src/scraper/model.py
+++ data_scrapers/src/scraper/model.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from ..model import CreatedAtMixin, UpdatedAtMixin
 
 
@@ -12,7 +12,8 @@ class Game(SQLModel, CreatedAtMixin, UpdatedAtMixin, table=True):
 
 class GameScreenshot(SQLModel, CreatedAtMixin, UpdatedAtMixin, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    # TODO: FK를 넣지 않을거야! 하지만 lazy key? 같은 옵션이 있는지 찾아봐
-    game_id: Optional[int] = Field(nullable=False)
     url: str = Field(max_length=2048, nullable=False)
     provider: str = Field(max_length=256)
+
+    game_id: Optional[int] = Field(nullable=False)
+    game: Optional[Game] = Relationship()
