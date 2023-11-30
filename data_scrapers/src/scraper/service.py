60c7b3fcb85c1faa36037d7a06ea45c6e6fcb810
2jun0 <soo28819@gmail.com>
2023-12-01 04:09:00 +0900

    feat: scrap games

---
+++ data_scrapers/src/scraper/service.py
@@ -0,0 +1,27 @@
+from .model import Game
+from . import steam_api
+
+
+def _put_kor_name_in_game(game: Game) -> None:
+    response = steam_api.get_game_details(game.steam_id, language="korean")
+
+    game_details = response[str(game.steam_id)]["data"]
+    kr_name = game_details["name"]
+
+    game.kr_name = kr_name
+
+
+def scrap_games() -> list[Game]:
+    games = []
+
+    # get top 100 games in 2 weeks
+    response = steam_api.get_top_100_games_in_2weeks()
+    for str_steam_id, detail in response.items():
+        game = Game(steam_id=int(str_steam_id), name=detail["name"])
+        games.append(game)
+
+    # update korean game name
+    for game in games:
+        _put_kor_name_in_game(game)
+
+    return games
