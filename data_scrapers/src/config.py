5e4974076aff4e8938322e5a8ffc818d34cdece8
2jun0 <soo28819@gmail.com>
2023-12-10 03:01:50 +0900

    feat: support env file in config

--- data_scrapers/src/config.py
+++ data_scrapers/src/config.py
@@ -1,5 +1,7 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
     DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
