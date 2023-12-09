632b40789af50b3287b7d16bd1e0d1f2d3a43e01
2jun0 <soo28819@gmail.com>
2023-12-09 16:56:47 +0900

    test: add test for scraping screenshot

--- data_scrapers/tests/scraper/test_service.py
+++ data_scrapers/tests/scraper/test_service.py
@@ -3,9 +3,9 @@ from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src.scraper.model import Game
+from src.scraper.model import Game, GameScreenshot
 from src.scraper.protocols import SteamAPI
-from src.scraper.service import scrap_games
+from src.scraper.service import scrap_game_screenshot, scrap_games
 from src.steam.model import SteamGameDetailResponse, SteamGameScreenshotResponse, TopSteamGameResponse
 from tests.factories import (
     SteamGameDetailResponseFactory,
@@ -16,10 +16,12 @@ from tests.factories import (
 
 class MockSteamAPI(SteamAPI):
     def __init__(self) -> None:
-        self.top_100_games = TopSteamGameResponseFactory.build_batch(100)
+        TopSteamGameResponseFactory.reset_sequence()
+        SteamGameDetailResponseFactory.reset_sequence()
+        SteamGameScreenshotResponseFactory.reset_sequence()
 
     def get_top_100_games_in_2weeks(self) -> list[TopSteamGameResponse]:
-        return self.top_100_games
+        return TopSteamGameResponseFactory.build_batch(100)
 
     def get_game_details(self, app_id: int, language: Optional[str] = None) -> SteamGameDetailResponse:
         return SteamGameDetailResponseFactory.build()
@@ -36,12 +38,28 @@ def test_scrap_geams는_100개의_게임을_저장한다(session: Session):
     assert len(scraped) == 100
 
 
-def test_scrap_games는_중복된_게임은_다시_저장하지_않는다(session: Session) -> None:
-    steam_api = MockSteamAPI()
-
-    scrap_games(steam_api, session)
-    scrap_games(steam_api, session)  # duplicated
+def test_scrap_games는_중복된_게임은_다시_저장하지_않는다(session: Session):
+    scrap_games(MockSteamAPI(), session)
+    scrap_games(MockSteamAPI(), session)  # duplicated
 
     scraped = session.scalars(select(Game)).all()
 
     assert len(scraped) == 100
+
+
+def test_scrap_screenshots은_스크린샷을_저장한다(session: Session, game: Game):
+    scrap_game_screenshot(MockSteamAPI(), session, game)
+
+    scraped = session.scalars(statement=select(GameScreenshot)).all()
+
+    assert len(scraped) > 0
+
+
+def test_scrap_screenshot은_중복된_스크린샷은_다시_저장하지_않는다(session: Session, game: Game):
+    scrap_game_screenshot(MockSteamAPI(), session, game)
+    before_scraped = session.scalars(statement=select(GameScreenshot)).all()
+
+    scrap_game_screenshot(MockSteamAPI(), session, game)
+    after_scraped = session.scalars(statement=select(GameScreenshot)).all()
+
+    assert len(before_scraped) == len(after_scraped)
