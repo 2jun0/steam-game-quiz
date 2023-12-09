47814b89a94f8075563025eb44b65e5b2fa2083f
2jun0 <soo28819@gmail.com>
2023-12-09 14:41:36 +0900

    fix: resolved remove existed models

--- data_scrapers/src/scraper/service.py
+++ data_scrapers/src/scraper/service.py
@@ -12,18 +12,20 @@ def _put_kor_name_in_game(steam_api: SteamAPI, game: Game) -> None:
     game.kr_name = game_detail.name
 
 
-def _remove_existed_games(session: Session, games: set[Game]) -> Iterable[Game]:
-    steam_ids = (game.steam_id for game in games)
-    exists = set(repository.get_games_in_steam_ids(session, steam_ids))
+def _remove_existed_games(session: Session, games: Iterable[Game]) -> set[Game]:
+    steam_id2game = {g.steam_id: g for g in games}
+    exists = set(g.steam_id for g in repository.get_games_in_steam_ids(session, steam_id2game.keys()))
 
-    return games - exists
+    return set(steam_id2game[steam_id] for steam_id in steam_id2game.keys() - exists)
 
 
-def _remove_existed_screenshot(session: Session, screenshots: set[GameScreenshot]) -> Iterable[GameScreenshot]:
-    steam_file_ids = (screenshot.steam_file_id for screenshot in screenshots)
-    exists = set(repository.get_game_screenshots_in_steam_file_ids(session, steam_file_ids))
+def _remove_existed_screenshot(session: Session, screenshots: Iterable[GameScreenshot]) -> set[GameScreenshot]:
+    file_id2screenshot = {s.steam_file_id: s for s in screenshots}
+    exists = set(
+        s.steam_file_id for s in repository.get_game_screenshots_in_steam_file_ids(session, file_id2screenshot.keys())
+    )
 
-    return screenshots - exists
+    return set(file_id2screenshot[file_id] for file_id in file_id2screenshot.keys() - exists)
 
 
 def scrap_games(steam_api: SteamAPI, session: Session) -> None:
@@ -34,7 +36,7 @@ def scrap_games(steam_api: SteamAPI, session: Session) -> None:
         games.append(Game(steam_id=g.app_id, name=g.name))
 
     # remove existed games
-    new_games = _remove_existed_games(session, set(games))
+    new_games = _remove_existed_games(session, games)
 
     # update korean game name
     for game in new_games:
@@ -51,7 +53,7 @@ def scrap_game_screenshot(steam_api: SteamAPI, session: Session, game: Game) -> 
         screenshots.append(GameScreenshot(steam_file_id=s.file_id, url=s.full_image_url))
 
     # remove existed screenshot
-    new_screenshots = _remove_existed_screenshot(session, set(screenshots))
+    new_screenshots = _remove_existed_screenshot(session, screenshots)
 
     session.add_all(new_screenshots)
 
