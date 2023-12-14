4a03b382a9d0f130269e618c1b30fc55d2671ba9
2jun0 <soo28819@gmail.com>
2023-12-14 18:38:35 +0900

    feat: missing files

---
+++ data_scrapers/public/aws_lambda/lambda_api.py
@@ -0,0 +1,47 @@
+import json
+from typing import Any, Iterable
+
+import boto3
+
+from ..model import Game, GameScreenshot, NewGame, NewGameScreenshot
+from .event import Event
+
+
+class LambdaAPI:
+    def __init__(self, private_function_name: str) -> None:
+        self.private_function_name = private_function_name
+        self.client = boto3.client("lambda")
+
+    def invoke_lambda(self, event: Event) -> Any:
+        response = self.client.invoke(FunctionName=self.private_function_name, Payload=json.dumps(event))
+
+        payload: Any = response["Payload"].read().decode("utf-8")
+        return json.loads(payload)
+
+    def get_some_games(self) -> list[Game]:
+        event = Event(name="get_some_games", payload=None)
+
+        games: list[dict[str, Any]] = self.invoke_lambda(event)
+        return [Game(**g) for g in games]
+
+    def get_games_in_steam_ids(self, steam_ids: Iterable[int]) -> list[Game]:
+        event = Event(name="get_games_in_steam_ids", payload=steam_ids)
+
+        games: list[dict[str, Any]] = self.invoke_lambda(event)
+        return [Game(**g) for g in games]
+
+    def get_screenshots_in_steam_file_ids(self, steam_file_ids: Iterable[int]) -> list[GameScreenshot]:
+        event = Event(name="get_screenshots_in_steam_file_ids", payload=steam_file_ids)
+
+        screenshots: list[dict[str, Any]] = self.invoke_lambda(event)
+        return [GameScreenshot(**s) for s in screenshots]
+
+    def save_games(self, games: Iterable[NewGame]):
+        event = Event(name="save_games", payload=games)
+
+        self.invoke_lambda(event)
+
+    def save_screenshots(self, screenshots: Iterable[NewGameScreenshot]):
+        event = Event(name="save_screenshots", payload=screenshots)
+
+        self.invoke_lambda(event)
---
+++ data_scrapers/public/scraper/service.py
@@ -0,0 +1,75 @@
+from typing import Iterable
+
+from ..aws_lambda.lambda_api import LambdaAPI
+from ..logger import logger
+from ..model import Game, NewGame, NewGameScreenshot
+from ..steam.exception import SteamAPINoContentsException
+from ..steam.steam_api import SteamAPI
+
+
+def _put_kor_name_in_new_game(steam_api: SteamAPI, game: NewGame) -> None:
+    try:
+        game_detail = steam_api.get_game_details(game.steam_id, language="korean")
+        game.kr_name = game_detail.name
+    except SteamAPINoContentsException:
+        pass  # game.kr_name will none
+
+
+def _remove_existed_new_games(lambda_api: LambdaAPI, games: Iterable[NewGame]) -> set[NewGame]:
+    steam_id2game = {g.steam_id: g for g in games}
+    exists = set(g.steam_id for g in lambda_api.get_games_in_steam_ids(steam_id2game.keys()))
+
+    return set(steam_id2game[steam_id] for steam_id in steam_id2game.keys() - exists)
+
+
+def _remove_existed_new_screenshot(
+    lambda_api: LambdaAPI, screenshots: Iterable[NewGameScreenshot]
+) -> set[NewGameScreenshot]:
+    file_id2screenshot = {s.steam_file_id: s for s in screenshots}
+    exists = set(s.steam_file_id for s in lambda_api.get_screenshots_in_steam_file_ids(file_id2screenshot.keys()))
+
+    return set(file_id2screenshot[file_id] for file_id in file_id2screenshot.keys() - exists)
+
+
+def scrap_games(steam_api: SteamAPI, lambda_api: LambdaAPI) -> None:
+    games: list[NewGame] = []
+
+    # get feature games
+    logger.info("getting feature games")
+    for g in steam_api.get_feature_games():
+        games.append(NewGame(steam_id=g.app_id, name=g.name))
+
+    # remove existed games
+    logger.info("removing existed games")
+    new_games = _remove_existed_new_games(lambda_api, games)
+
+    # update korean game name
+    logger.info("updating korean game name")
+    for game in new_games:
+        _put_kor_name_in_new_game(steam_api, game)
+
+    # save new games
+    lambda_api.save_games(new_games)
+
+
+def scrap_game_screenshot(steam_api: SteamAPI, lambda_api: LambdaAPI, game: Game) -> None:
+    # get some screenshots
+    logger.info("getting some screenshots")
+    screenshots: list[NewGameScreenshot] = []
+
+    for s in steam_api.get_game_screenshots(game.steam_id):
+        screenshots.append(NewGameScreenshot(steam_file_id=s.file_id, url=s.full_image_url, game_id=game.id))
+
+    # remove existed screenshot
+    logger.info("removing existed screenshots")
+    new_screenshots = _remove_existed_new_screenshot(lambda_api, screenshots)
+
+    # save new screenshots
+    lambda_api.save_screenshots(new_screenshots)
+
+
+def scrap_game_screenshot_for_all(steam_api: SteamAPI, lambda_api: LambdaAPI) -> None:
+    games = lambda_api.get_some_games()
+
+    for game in games:
+        scrap_game_screenshot(steam_api, lambda_api, game)
