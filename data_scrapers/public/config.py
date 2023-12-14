e638e556e9697f3412b35111daffa88b1bdc697b
2jun0 <soo28819@gmail.com>
2023-12-15 01:55:36 +0900

    feat: remove config for public

--- data_scrapers/public/config.py
+++
@@ -1,5 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file="pred.env", env_file_encoding="utf-8")
