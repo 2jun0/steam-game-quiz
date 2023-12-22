01a70a36d2869e308e821ede592bad310bd620ed
2jun0 <soo28819@gmail.com>
2023-12-22 15:49:55 +0900

    feat: add quiz service dependency

---
+++ backend/src/dependency.py
@@ -0,0 +1,8 @@
+from typing import Generator
+from sqlmodel import Session
+from .database import engine
+
+
+def get_session() -> Generator[Session, None]:
+    with Session(engine) as session:
+        yield session
---
+++ backend/src/quiz/dependency.py
@@ -0,0 +1,9 @@
+from fastapi import Depends
+from sqlmodel import Session
+
+from ..dependency import get_session
+from .service import QuizService
+
+
+def get_quiz_service(session: Session = Depends(get_session())) -> QuizService:
+    return QuizService(session)
