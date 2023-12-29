653ca3c4d70e151983ec0ea543b48c1ce47397c3
2jun0 <soo28819@gmail.com>
2023-12-29 23:22:12 +0900

    refactor: Session의 사용 영역을 확장함

--- backend/tests/utils/game.py
+++ backend/tests/utils/game.py
@@ -2,7 +2,6 @@ from asyncio import Lock
 
 from sqlmodel import Session
 
-from src.database import engine
 from src.game.model import Game
 
 from .utils import random_kr_string, random_name
@@ -11,16 +10,15 @@ steam_id_counter = 0
 steam_id_lock = Lock()
 
 
-async def create_random_game() -> Game:
+async def create_random_game(session: Session) -> Game:
     global steam_id_counter
 
-    with Session(engine) as session:
-        async with steam_id_lock:
-            steam_id_counter += 1
-            game = Game(steam_id=steam_id_counter, name=random_name(), kr_name=random_kr_string())
+    async with steam_id_lock:
+        steam_id_counter += 1
+        game = Game(steam_id=steam_id_counter, name=random_name(), kr_name=random_kr_string())
 
-        session.add(game)
-        session.commit()
-        session.refresh(game)
+    session.add(game)
+    session.commit()
+    session.refresh(game)
 
     return game
--- backend/tests/utils/screenshot.py
+++ backend/tests/utils/screenshot.py
@@ -2,7 +2,6 @@ from asyncio import Lock
 
 from sqlmodel import Session
 
-from src.database import engine
 from src.game.model import GameScreenshot
 
 from .game import create_random_game
@@ -12,21 +11,20 @@ steam_file_id_counter = 0
 steam_file_id_lock = Lock()
 
 
-async def create_random_game_screenshot(game_id: int | None = None) -> GameScreenshot:
+async def create_random_game_screenshot(session: Session, *, game_id: int | None = None) -> GameScreenshot:
     global steam_file_id_counter
 
     if game_id is None:
-        game = await create_random_game()
+        game = await create_random_game(session)
         assert game.id is not None
         game_id = game.id
 
-    with Session(engine) as session:
-        async with steam_file_id_lock:
-            steam_file_id_counter += 1
-            screenshot = GameScreenshot(steam_file_id=steam_file_id_counter, url=random_image_url(), game_id=game_id)
+    async with steam_file_id_lock:
+        steam_file_id_counter += 1
+        screenshot = GameScreenshot(steam_file_id=steam_file_id_counter, url=random_image_url(), game_id=game_id)
 
-        session.add(screenshot)
-        session.commit()
-        session.refresh(screenshot)
+    session.add(screenshot)
+    session.commit()
+    session.refresh(screenshot)
 
     return screenshot
