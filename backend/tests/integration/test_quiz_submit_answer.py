b9e112cc8a757424de96f4f1326f47886fc0316e
2jun0 <soo28819@gmail.com>
2024-01-01 00:42:59 +0900

    test: /quiz/submit_answer의 json body형식 변경

--- backend/tests/integration/test_quiz_submit_answer.py
+++ backend/tests/integration/test_quiz_submit_answer.py
@@ -10,7 +10,7 @@ from tests.utils.quiz import create_random_quiz
 async def test_post_submit_true_answer(client: TestClient, session: Session):
     saved_quiz = await create_random_quiz(session)
 
-    res = await client.post("/quiz/submit_answer", json={"quiz_id": saved_quiz.id, "game_name": saved_quiz.game.name})
+    res = await client.post("/quiz/submit_answer", json={"quiz_id": saved_quiz.id, "answer": saved_quiz.game.name})
     assert res.status_code == status.HTTP_200_OK
 
     res_json = res.json()
@@ -21,7 +21,7 @@ async def test_post_submit_true_answer(client: TestClient, session: Session):
 async def test_post_submit_false_answer(client: TestClient, session: Session):
     saved_quiz = await create_random_quiz(session)
 
-    res = await client.post("/quiz/submit_answer", json={"quiz_id": saved_quiz.id, "game_name": "빙빙바리바리구"})
+    res = await client.post("/quiz/submit_answer", json={"quiz_id": saved_quiz.id, "answer": "빙빙바리바리구"})
     assert res.status_code == status.HTTP_200_OK
 
     res_json = res.json()
@@ -32,5 +32,5 @@ async def test_post_submit_false_answer(client: TestClient, session: Session):
 async def test_post_submit_answer_with_invalid_quiz_id(client: TestClient, session: Session):
     saved_quiz = await create_random_quiz(session)
 
-    res = await client.post("/quiz/submit_answer", json={"quiz_id": -1, "game_name": saved_quiz.game.name})
+    res = await client.post("/quiz/submit_answer", json={"quiz_id": -1, "answer": saved_quiz.game.name})
     assert res.status_code == status.HTTP_404_NOT_FOUND
