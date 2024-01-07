fd1f80bee69579030fb81f1275a7fe87626ba36b
2jun0 <soo28819@gmail.com>
2024-01-07 15:10:54 +0900

    fix: 실행시 테이블을 생성하지 않음

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,10 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import SQLModel
 
 from .config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)  # type: ignore
-
-
-def create_all_table():
-    SQLModel.metadata.create_all(engine.sync_engine)
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
 
 from src.config import app_configs
-from src.database import create_all_table
 from src.game.router import router as game_router
 from src.quiz.router import router as quiz_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_all_table()
     yield
 
 
