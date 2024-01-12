78293277ad1dad1abb5fc4e08dde09144de30bca
2jun0 <soo28819@gmail.com>
2024-01-12 22:10:42 +0900

    test: 로그인 / 회원가입 / 게스트 회원가입 테스트 추가

---
+++ backend/tests/integration/test_auth.py
@@ -0,0 +1,21 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+
+def test_new_guest(client: TestClient, session: Session):
+    res = client.post("/auth/quest/new")
+    assert res.status_code == status.HTTP_201_CREATED
+
+    res_json = res.json()
+    assert "guest_id" in res_json
+
+
+def test_login(client: TestClient, session: Session):
+    res = client.post("/auth/login")
+    assert res.status_code == status.HTTP_200_OK
+
+
+def test_register(client: TestClient, session: Session):
+    res = client.post("/auth/register", json={"id": "vkdlTjs10", "password": "python:10"})
+    assert res.status_code == status.HTTP_201_CREATED
