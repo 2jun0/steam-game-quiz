c9d109761978f4fd3e35d90af68a93fba365b66d
2jun0 <soo28819@gmail.com>
2024-01-15 01:53:55 +0900

    feat: user 테이블과 oauthaccount 테이블의 연관관계 설정

--- backend/src/auth/model.py
+++ backend/src/auth/model.py
@@ -1,17 +1,21 @@
 from fastapi_users_db_sqlmodel import SQLModelBaseOAuthAccount, SQLModelBaseUserDB
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 from ..model import CreatedAtMixin, UpdatedAtMixin
-
-
-class OAuthAccount(CreatedAtMixin, UpdatedAtMixin, SQLModelBaseOAuthAccount, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
 
 
 class User(CreatedAtMixin, UpdatedAtMixin, SQLModelBaseUserDB, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(max_length=20)
+    oauth_accounts: list["OAuthAccount"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"lazy": "selectin", "cascade": "all, delete"}
+    )
+
+
+class OAuthAccount(CreatedAtMixin, UpdatedAtMixin, SQLModelBaseOAuthAccount, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="oauth_accounts")
 
 
 class GuestUser(CreatedAtMixin, UpdatedAtMixin, SQLModel, table=True):
