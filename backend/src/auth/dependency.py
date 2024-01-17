f02a4b62e96463ca3cd5551830ebf685fa80d1a2
2jun0 <soo28819@gmail.com>
2024-01-17 17:40:16 +0900

    refactor: dependency의 명명규칙 맞춤

--- backend/src/auth/dependency.py
+++ backend/src/auth/dependency.py
@@ -7,4 +7,4 @@ from .user import fastapi_users
 
 current_active_verified_user = fastapi_users.current_user(active=True, verified=True)
 
-CURRENT_USER = Annotated[User, Depends(current_active_verified_user)]
+CURRENT_USER_DEP = Annotated[User, Depends(current_active_verified_user)]
