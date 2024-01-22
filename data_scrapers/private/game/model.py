4aadc972359f71860df990a5321920f954f7b066
2jun0 <soo28819@gmail.com>
2024-01-22 15:06:11 +0900

    feat: Game의 스키마 변경 + Genre 추가

--- data_scrapers/private/game/model.py
+++ data_scrapers/private/game/model.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
-from sqlalchemy import String
+from sqlalchemy import Column, ForeignKey, String, Table
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..model import Base, CreatedAtMixin, UpdatedAtMixin
@@ -15,6 +15,7 @@ class Game(CreatedAtMixin, UpdatedAtMixin, Base):
     steam_id: Mapped[int] = mapped_column(unique=True)
     name: Mapped[str] = mapped_column(String(64))
     kr_name: Mapped[Optional[str]] = mapped_column(String(64))
+    owners: Mapped[int] = mapped_column()
 
     def __repr__(self) -> str:
         return f"Game(id={self.id}, steam_id={self.steam_id}, name={self.name}, kr_name={self.kr_name})"
@@ -37,3 +38,12 @@ class GameDto(BaseModel):
     kr_name: Optional[str]
     created_at: datetime
     updated_at: datetime
+
+
+game_genre_link = Table(
+    "game_genre_link",
+    Base.metadata,
+    Column("id", primary_key=True),
+    Column("game_id", ForeignKey("game.id")),
+    Column("genre.id", ForeignKey("genre.id")),
+)
