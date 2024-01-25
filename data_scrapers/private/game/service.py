9d55c269e31e7c0599ec17fbcae5287820bb73f2
2jun0 <soo28819@gmail.com>
2024-01-25 15:40:47 +0900

    feat: 장르 캐싱

--- data_scrapers/private/game/service.py
+++ data_scrapers/private/game/service.py
@@ -3,9 +3,12 @@ from typing import Any, Sequence
 
 from sqlalchemy.orm import Session
 
+from ..genre.model import Genre
 from ..genre.repository import get_genre
 from . import repository
 from .model import Game
+
+genres: dict[str, Genre] = {}
 
 
 def get_some_games(session: Session) -> list[dict[str, Any]]:
@@ -14,21 +17,28 @@ def get_some_games(session: Session) -> list[dict[str, Any]]:
     return [g.to_dto().model_dump(mode="json") for g in some_games]
 
 
+def _get_genres(session: Session, name: str) -> Genre:
+    if name not in genres:
+        genre = get_genre(session, name=name)
+        if genre is None:
+            genre = Genre(name=name)
+        genres[name] = genre
+
+    return genres[name]
+
+
 def save_games(session: Session, games: Sequence[dict[str, Any]]):
     for game in games:
-        genres = []
+        game_genres = []
 
         for genre_name in game["genres"]:
-            genre = get_genre(session, name=genre_name)
+            game_genres.append(_get_genres(session, genre_name))
 
-            if genre is not None:
-                genres.append(genre)
-
-        del game["genres"]
+        game["genres"] = game_genres
 
         game["released_at"] = datetime.fromtimestamp(game["released_at"])
 
-        session.add(Game(**game, genres=genres))
+        session.add(Game(**game))
 
 
 def get_games_in_steam_ids(session: Session, steam_ids: Sequence[int]) -> list[dict[str, Any]]:
