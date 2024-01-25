8b06fb99f3b5d0c4f38eabf8f9464ec144c384b1
2jun0 <soo28819@gmail.com>
2024-01-25 16:50:54 +0900

    chore: private lambda의 .env 파일 이름 수정

--- data_scrapers/private/config.py
+++ data_scrapers/private/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     DATABASE_URL: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".database.env", env_file_encoding="utf-8")
 
 
 setting = Config()  # type: ignore
