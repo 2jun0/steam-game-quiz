3b28fc456a104a52c2d17adb7a9d6c083c950d61
2jun0 <soo28819@gmail.com>
2024-01-25 16:49:52 +0900

    feat: 스크린샷 스크래핑 비활성화

--- data_scrapers/public/lambda_func.py
+++ data_scrapers/public/lambda_func.py
@@ -26,4 +26,4 @@ def lambda_handler(event: Any, context: Any):
     lambda_api = LambdaAPI("private")
 
     scrap_games_job(lambda_api)
-    scrap_screenshots_job(lambda_api)
+    # scrap_screenshots_job(lambda_api)
