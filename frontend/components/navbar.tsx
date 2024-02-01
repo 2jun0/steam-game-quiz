ef0f1d63998990a086730bdd5664368b91ed6471
2jun0 <soo28819@gmail.com>
2024-02-01 17:14:20 +0900

    feat: 퀴즈 정답 제출 화면단에서 구현

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -19,12 +19,6 @@ import NextLink from "next/link";
 import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-	TwitterIcon,
-	GithubIcon,
-	DiscordIcon,
-	HeartFilledIcon,
-} from "@/components/icons";
 
 import { Logo } from "@/components/icons";
 import { LoginModal } from "./login-modal";
@@ -62,7 +56,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
+						<p className="font-bold text-inherit">Game Detective</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -88,15 +82,6 @@ export const Navbar = () => {
 				justify="end"
 			>
 				<NavbarItem className="hidden sm:flex gap-2">
-					{/* <Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-						<TwitterIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-						<DiscordIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.github} aria-label="Github">
-						<GithubIcon className="text-default-500" />
-					</Link> */}
 					<ThemeSwitch />  
 				</NavbarItem>
 				<NavbarItem className="md:flex">
@@ -107,7 +92,6 @@ export const Navbar = () => {
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
 				<ThemeSwitch />
 				{loginLogout}
-				{/* <NavbarMenuToggle /> */}
 			</NavbarContent>
 
 			{/* <NavbarMenu>
