1d2aa4e04232741c268200c357be2f7332becdca
2jun0 <soo28819@gmail.com>
2024-02-07 23:16:09 +0900

    test: genres pick의 아웃풋 개수 검증시 "유일한" 개수 검증

--- data_scrapers/tests/daily_quiz/daily_quiz/test_genre_picker.py
+++ data_scrapers/tests/daily_quiz/daily_quiz/test_genre_picker.py
@@ -5,10 +5,10 @@ from daily_quiz.daily_quiz.genre_picker import pick_genres
 
 
 @pytest.mark.parametrize("genre_cnt", (5, 10))
-def test_pick_genres는_입력된_개수만큼_장르를_반환해야_한다(genre_cnt: int):
+def test_pick_genres는_입력된_개수만큼_유일한_장르_개수를_반환해야_한다(genre_cnt: int):
     genres = pick_genres(genre_cnt)
 
-    assert len(genres) == genre_cnt
+    assert len(set(genres)) == genre_cnt
 
 
 @pytest.mark.parametrize("setting_genres", (tuple(setting.GAME_GENERES), ("Adventure", "RPG")))
