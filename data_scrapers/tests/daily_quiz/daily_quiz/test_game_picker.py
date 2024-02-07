91dc619e1109d0c6280558b537e55581ab5cd573
2jun0 <soo28819@gmail.com>
2024-02-07 23:28:19 +0900

    test: older game cnt 개수 관련 테스트 추가

--- data_scrapers/tests/daily_quiz/daily_quiz/test_game_picker.py
+++ data_scrapers/tests/daily_quiz/daily_quiz/test_game_picker.py
@@ -1,3 +1,8 @@
+import random
+
+import pytest
+
+from daily_quiz.config import setting
 from daily_quiz.daily_quiz.game_picker import pick_games
 from tests.daily_quiz.utils.model import create_random_game
 
@@ -18,6 +23,26 @@ def test_pick_games은_입력한_게임에서_장르별로_1개씩_선택해야_
         create_random_game(genres=["Massively Multiplayer", "Strategy"]),
         create_random_game(genres=["Strategy", "RPG"]),
     ]
+
+    picked = pick_games(games, genres)
+
+    assert len(picked) == len(genres)
+
+
+@pytest.mark.parametrize("older_cnt", (0, 2, 5))
+def test_pick_games은_older개수_설정이_변경되어도_적응해야한다(older_cnt: int):
+    setting.OLDER_GAME_COUNT = older_cnt
+
+    genres = [
+        "Action",
+        "Adventure",
+        "Massively Multiplayer",
+        "Strategy",
+        "RPG",
+    ]
+    games = []
+    for k in range(1, 4):
+        games.extend([create_random_game(genres=random.sample(genres, k=k)) for i in range(100)])
 
     picked = pick_games(games, genres)
 
