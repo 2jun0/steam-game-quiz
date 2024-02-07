1fa5f94b23dd70dc06231a6ee4954baa6134d574
2jun0 <soo28819@gmail.com>
2024-02-07 19:14:21 +0900

    test: game_picker 테스트 추가

--- data_scrapers/tests/daily_quiz/daily_quiz/test_game_picker.py
+++ data_scrapers/tests/daily_quiz/daily_quiz/test_game_picker.py
@@ -1,2 +1,24 @@
-def test_pick_games은():
-    dfse
+from daily_quiz.daily_quiz.game_picker import pick_games
+from tests.daily_quiz.utils.model import create_random_game
+
+
+def test_pick_games은_입력한_게임에서_장르별로_1개씩_선택해야_한다():
+    genres = [
+        "Action",
+        "Adventure",
+        "Massively Multiplayer",
+        "Strategy",
+        "RPG",
+    ]
+    games = [
+        create_random_game(genres=["Action", "Adventure"]),
+        create_random_game(genres=["Action", "Massively Multiplayer"]),
+        create_random_game(genres=["Adventure", "Massively Multiplayer"]),
+        create_random_game(genres=["Adventure", "Strategy"]),
+        create_random_game(genres=["Massively Multiplayer", "Strategy"]),
+        create_random_game(genres=["Strategy", "RPG"]),
+    ]
+
+    picked = pick_games(games, genres)
+
+    assert len(picked) == len(genres)
