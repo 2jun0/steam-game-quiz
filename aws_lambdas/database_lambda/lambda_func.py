7176f7f7a1c7ab8b39c6cc19e607ad56f5f6e985
2jun0 <soo28819@gmail.com>
2024-02-08 21:59:37 +0900

    fix: database lambda에서 event handler에서 payload 없는 로직 수정

--- aws_lambdas/database_lambda/lambda_func.py
+++ aws_lambdas/database_lambda/lambda_func.py
@@ -1,33 +1,33 @@
 from typing import Any, Callable
 
-from sqlalchemy.orm import Session
 
-from database_lambda.database import engine
-from database_lambda.event import Event, EventName
-from database_lambda.game.service import get_all_games, save_games
-from database_lambda.logger import logger
-from database_lambda.screenshot.service import save_screenshots
+def lambda_handler(event: Any, context: Any):
 
-funcs: dict[EventName, Callable[..., Any]] = {
-    "save_games": save_games,
-    "save_screenshots": save_screenshots,
-    "get_all_games": get_all_games,
-}
+    from sqlalchemy.orm import Session
 
+    from database_lambda.database import engine
+    from database_lambda.event import Event, EventName
+    from database_lambda.game.service import get_all_games, save_games
+    from database_lambda.logger import logger
+    from database_lambda.screenshot.service import save_screenshots
 
-def handle_event(session: Session, event: Event) -> Any:
-    func = funcs[event["name"]]
+    funcs: dict[EventName, Callable[..., Any]] = {
+        "save_games": save_games,
+        "save_screenshots": save_screenshots,
+        "get_all_games": get_all_games,
+    }
 
-    if "payload" in event:
-        result = func(session, event["payload"])
-    else:
-        result = func(session)
+    def handle_event(session: Session, event: Event) -> Any:
+        func = funcs[event["name"]]
 
-    session.commit()
-    return result
+        if "payload" in event and event["payload"]:
+            result = func(session, event["payload"])
+        else:
+            result = func(session)
 
+        session.commit()
+        return result
 
-def lambda_handler(event: Event, context: Any):
     logger.info("Handle event [required event is %s]", event)
 
     with Session(engine) as session:
