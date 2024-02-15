ccdae1d571eabc8b65bbdb52f75cdacca6b9919b
2jun0 <soo28819@gmail.com>
2024-02-15 23:04:50 +0900

    feat: 프론트 타이틀 수정

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,46 +1,12 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Steam Game Quiz",
+	description: "Challenge the quiz about Steam games!",
 	navItems: [
 		{
 			label: "Home",
 			href: "/",
-		},
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
 		},
 	],
 	links: {
@@ -48,6 +14,5 @@ export const siteConfig = {
 		twitter: "https://twitter.com/getnextui",
 		docs: "https://nextui.org",
 		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
 	},
 };
