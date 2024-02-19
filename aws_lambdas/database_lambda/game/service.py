e7f646b31c3b4bc66c7893d21849b9589107a76a
2jun0 <soo28819@gmail.com>
2024-02-20 02:08:15 +0900

    feat: save_games 이벤트 핸들러 함수 인자 수정

--- aws_lambdas/database_lambda/game/service.py
+++ aws_lambdas/database_lambda/game/service.py
@@ -1,9 +1,9 @@
 from collections.abc import Iterable
 from typing import Any
 
-from elasticsearch import Elasticsearch
 from sqlalchemy.orm import Session
 
+from ..es import es_client
 from . import es, repository
 from .model_factory import to_models
 from .schema import SaveGame
@@ -15,7 +15,7 @@ def get_all_games(session: Session) -> list[dict[str, Any]]:
     return [g.to_dto().model_dump(mode="json") for g in some_games]
 
 
-def save_games(session: Session, es_client: Elasticsearch, games: Iterable[SaveGame]):
+def save_games(session: Session, games: Iterable[SaveGame]):
     models = to_models(session, games)
     session.add_all(models)
     es.save_docs(es_client, games=models)
