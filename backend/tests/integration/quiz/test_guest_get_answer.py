a74932744831b224a08bfaa18a5cea159b05f491
2jun0 <soo28819@gmail.com>
2024-02-21 01:29:36 +0900

    tests: 게스트를 위한 퀴즈 제출 테스트

---
+++ backend/tests/integration/quiz/test_guest_get_answer.py
@@ -0,0 +1,44 @@
+import json
+from uuid import uuid4
+
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from tests.utils.quiz import create_random_quiz
+from tests.utils.utils import jsontime2datetime
+
+
+def test_get_guest_answer(session: Session, client: TestClient):
+    quiz = create_random_quiz(session)
+    wrong_answers = ["빙빙바리바리구1", "빙빙바리바리구2", "빙빙바리바리구3"]
+
+    guest = {
+        "id": str(uuid4()),
+        "quiz_answers": {
+            quiz.id: [
+                {"answer": answer, "correct": False, "created_at": "2024-02-20T16:02:01.816Z"}
+                for answer in wrong_answers
+            ]
+        },
+    }
+
+    res = client.get(
+        f"/quiz/guest/answer?quiz_id={quiz.id}",
+        cookies={"guest": json.dumps(guest)},
+    )
+    assert res.status_code == status.HTTP_200_OK
+    res_json = res.json()
+
+    # 제출(저장) 순서도 동일해야 한다.
+    for quiz_ans_res, quiz_ans in zip(res_json["quiz_answers"], guest["quiz_answers"][quiz.id]):
+        assert quiz_ans_res["answer"] == quiz_ans["answer"]
+        assert quiz_ans_res["correct"] == quiz_ans["correct"]
+        assert jsontime2datetime(quiz_ans_res["created_at"]) == jsontime2datetime(quiz_ans["created_at"])
+
+
+def test_get_guest_answer_with_not_existed_quiz_id(client: TestClient):
+    quiz_id = 1
+
+    res = client.get(f"/quiz/guest/answer?quiz_id={quiz_id}")
+    assert res.status_code == status.HTTP_404_NOT_FOUND
---
+++ backend/tests/integration/quiz/test_guest_submit_answer.py
@@ -0,0 +1,97 @@
+import json
+from uuid import uuid4
+
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from src.config import settings
+from tests.utils.quiz import create_random_quiz
+
+
+def test_post_guest_submit_true_answer(client: TestClient, session: Session):
+    quiz = create_random_quiz(session)
+
+    res = client.post("/quiz/guest/submit_answer", json={"quiz_id": quiz.id, "answer": quiz.game.name})
+    assert res.status_code == status.HTTP_200_OK
+
+    res_json = res.json()
+    assert res_json["correct"] is True
+
+    guest = json.loads(res.cookies["guest"])
+
+    quiz_answer = guest["quiz_answers"][0]
+    assert quiz_answer["answer"] == quiz.game.name
+    assert quiz_answer["correct"] is True
+
+
+def test_post_guest_submit_false_answer(client: TestClient, session: Session):
+    quiz = create_random_quiz(session)
+    wrong_answers = ["빙빙바리바리구1", "빙빙바리바리구2", "빙빙바리바리구3"]
+
+    guest = {
+        "id": str(uuid4()),
+        "quiz_answers": {},
+    }
+
+    for wrong_answer in wrong_answers:
+        res = client.post(
+            "/quiz/guest/submit_answer",
+            json={"quiz_id": quiz.id, "answer": wrong_answer},
+            cookies={"guest": json.dumps(guest)},
+        )
+        assert res.status_code == status.HTTP_200_OK
+
+        res_json = res.json()
+        assert res_json["correct"] is False
+
+        guest = json.loads(res.cookies["guest"])
+
+        quiz_answer = guest["quiz_answers"][0]
+        assert quiz_answer["answer"] == quiz.game.name
+        assert quiz_answer["correct"] is True
+
+
+def test_post_guest_submit_answer_with_not_existed_quiz_id(client: TestClient):
+    res = client.post("/quiz/guest/submit_answer", json={"quiz_id": -1, "answer": "아무거나 빙빙바리바리구"})
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_post_guest_submit_answer_with_exceed_submission_limit(client: TestClient, session: Session):
+    quiz = create_random_quiz(session)
+    wrong_answer = "빙빙바리바리구"
+
+    guest = {
+        "id": str(uuid4()),
+        "quiz_answers": {
+            quiz.id: [
+                {"answer": wrong_answer, "correct": False, "created_at": "2024-02-20T16:02:01.816Z"}
+                for _ in range(settings.QUIZ_ANSWER_SUBMISSION_LIMIT)
+            ]
+        },
+    }
+
+    res = client.post(
+        "/quiz/guest/submit_answer",
+        json={"quiz_id": quiz.id, "answer": "아무거나 방방빙방"},
+        cookies={"guest": json.dumps(guest)},
+    )
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_post_guest_submit_answer_with_prior_correct_answer(client: TestClient, session: Session):
+    quiz = create_random_quiz(session)
+
+    guest = {
+        "id": str(uuid4()),
+        "quiz_answers": {
+            quiz.id: [{"answer": quiz.game.name, "correct": True, "created_at": "2024-02-20T16:02:01.816Z"}]
+        },
+    }
+
+    res = client.post(
+        "/quiz/guest/submit_answer",
+        json={"quiz_id": quiz.id, "answer": "아무거나 방방빙방"},
+        cookies={"guest": json.dumps(guest)},
+    )
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
