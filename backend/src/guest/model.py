5893faff76b217fa37fcc387d6a4c31d53c243f0
2jun0 <soo28819@gmail.com>
2024-02-20 17:44:57 +0900

    feat: remove guest model

--- backend/src/guest/model.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-from ..model import CreatedAtMixin, UpdatedAtMixin
-
-
-class Guest(CreatedAtMixin, UpdatedAtMixin, SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
