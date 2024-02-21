619ebcf6cfa31b839dc9727241d1bd8b2c257e4b
2jun0 <soo28819@gmail.com>
2024-02-21 23:13:07 +0900

    test: save_games에 대한 es docs 업데이트 테스트

--- aws_lambdas/tests/database_lambda/game/test_game_service.py
+++ aws_lambdas/tests/database_lambda/game/test_game_service.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from database_lambda.game.model import Game
 from database_lambda.game.schema import SaveGame
 from database_lambda.game.service import get_all_games, save_games
-from tests.database_lambda.utils.game import create_random_game
+from tests.database_lambda.utils.game import create_random_game, search_game_docs
 from tests.database_lambda.utils.utils import random_datetime
 
 
@@ -35,9 +35,14 @@ def test_save_games은_입력한_게임을_저장해야_한다(session: Session,
     ]
     save_games(games, session=session, es_client=es_client)
 
-    saved = session.scalars(select(Game)).all()
+    # check rdb
+    models = session.scalars(select(Game)).all()
+    assert set(g["steam_id"] for g in games) == set(g.steam_id for g in models)
 
-    assert set(g["steam_id"] for g in games) == set(g.steam_id for g in saved)
+    # check es
+    docs = search_game_docs(es_client)
+    assert set(g["name"] for g in games) == set(g["_source"]["name"] for g in docs)
+    assert set(g.id for g in models) == set(g["_source"]["id"] for g in docs)
 
 
 def test_save_games은_이미_저장한_게임을_중복저장하지_않는다(session: Session, es_client: Elasticsearch):
@@ -60,11 +65,14 @@ def test_save_games은_이미_저장한_게임을_중복저장하지_않는다(s
 
     save_games(games, session=session, es_client=es_client)
     before_saved = session.scalars(select(Game)).all()
+    before_docs = search_game_docs(es_client)
 
     save_games(games, session=session, es_client=es_client)
     after_saved = session.scalars(select(Game)).all()
+    after_docs = search_game_docs(es_client)
 
     assert before_saved == after_saved
+    assert before_docs == after_docs
 
 
 def test_save_games은_이미_저장한_게임은_업데이트_한다(session: Session, es_client: Elasticsearch):
@@ -85,9 +93,14 @@ def test_save_games은_이미_저장한_게임은_업데이트_한다(session: S
 
     save_games([before_game], session=session, es_client=es_client)
     save_games([after_game], session=session, es_client=es_client)
-    saved = session.scalars(select(Game)).one().to_dto()
 
+    # check rdb
+    saved = session.scalars(select(Game)).one().to_dto()
     assert saved.name == after_game["name"]
     assert saved.kr_name == after_game["kr_name"]
-    assert saved.released_at.timestamp() == after_game["released_at"]
     assert saved.genres == after_game["genres"]
+
+    # check es
+    docs = search_game_docs(es_client)
+    assert len(docs) == 1
+    assert docs[0]["name"] == after_game["name"]
