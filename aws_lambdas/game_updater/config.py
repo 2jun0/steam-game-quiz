b0b774319a198fc9ddaa403569a6d4c677d45662
2jun0 <soo28819@gmail.com>
2024-03-11 17:25:59 +0900

    feat: igdb api 구현

--- aws_lambdas/game_updater/config.py
+++ aws_lambdas/game_updater/config.py
@@ -2,9 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
+    DATABASE_LAMBDA_NAME: str = "database"
+
     WORKER_CNT: int = 10
     MIN_REVENUE: int = 10000000  # 10M
-    DATABASE_LAMBDA_NAME: str = "database"
+
+    IGDB_CLIENT_ID: str
+    IGDB_CLIENT_SECRET: str
 
     model_config = SettingsConfigDict(env_file=".scraper.env", env_file_encoding="utf-8")
 
