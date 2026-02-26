39b4c3a3fcc637a687593fe6ed58d41f7dcfa203
2jun0 <soo28819@gmail.com>
2026-02-27 00:09:38 +0900

    test: 성적인 게임을 구하기 않는다는 테스트 삭제.

--- aws_lambdas/tests/game_updater/scraper/test_scrap_game.py
+++ aws_lambdas/tests/game_updater/scraper/test_scrap_game.py
@@ -28,13 +28,3 @@ def test_scrap_games는_수익이_높은_게임만_구한다(mock_steam_api: Moc
 
     assert len(scraped) == 1
     assert scraped[0].steam_id == popular_game["steam_id"]
-
-
-@pytest.mark.parametrize("sex_tag", ("Sexual Content", "NSFW"))
-def test_scrap_games는_성적인_게임을_구하지_않는다(mock_steam_api: MockSteamAPI, sex_tag: str):
-    sexual_game = create_random_game(revenue=setting.MIN_REVENUE, tags=[sex_tag])
-    mock_steam_api.add_mock_game(sexual_game)
-
-    scraped = scrap_games(mock_steam_api)
-
-    assert len(scraped) == 0
